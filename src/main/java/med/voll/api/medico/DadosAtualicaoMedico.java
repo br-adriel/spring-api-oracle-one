c987287c042e1614da4bbde88c41803efef9f61e
Adriel <adriel.fsantos@outlook.com>
2024-06-05 17:41:25 -0300

    feat: Add DadosAtualicaoMedico record

---
+++ src/main/java/med/voll/api/medico/DadosAtualicaoMedico.java
@@ -0,0 +1,17 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualicaoMedico(
+        @NotNull
+        Long id,
+
+        String nome,
+        String telefone,
+
+        @Valid
+        DadosEndereco endereco
+        ) {
+}
