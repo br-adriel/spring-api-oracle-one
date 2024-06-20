21d09ed7c662b807f71fbf3ea0b5d357b07151a7
Adriel <adriel.fsantos@outlook.com>
2024-06-19 22:34:46 -0300

    feat: Add test for ConsultaController

---
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ConsultaControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve retornar o código 400 quando as informações são inválidas")
+    @WithMockUser
+    void agendar_cenario1() throws Exception {
+        var res = mvc.perform(post("/consultas")).andReturn().getResponse();
+        assertThat(res.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
